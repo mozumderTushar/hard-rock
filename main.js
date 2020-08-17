6fad36ab906268ae39e251c734ec43a6f6085e31
mozumderTushar <56648029+mozumderTushar@users.noreply.github.com>
2020-08-17 21:37:17 +0600

    bugs fixed

--- main.js
+++ main.js
@@ -1,39 +1,28 @@
-//searchBtn
-
+//clicked added searchBtn
 document.getElementById('searchBtn').addEventListener('click',function(){
 
         const searchInput = document.getElementById('searchInput').value;
-        // console.log(searchInput);
 
         fetch(`https://api.lyrics.ovh/suggest/${searchInput}`)
         .then(res => res.json())
         .then(data => {
-            
             displaySong(data);
         })
 })
 
+//display searched song
 const displaySong = (data)=>{
-    // console.log(data);
-
     const simpleResult = document.getElementById('simpleResult');
    
-  
-
     for (let i = 0; i < 10; i++) {
         
              const songTitle = data.data[i].album.title;
-            //  console.log(songTitle);
-
              const songArtist = data.data[i].artist.name;
-            //  console.log(songArtist);
 
-            //  simpleResult.innerHtml += ` <p class="author lead"><strong>${songTitle}</strong> Album by <span>${songArtist}</span> <button class="btn btn-success">Get Lyrics</button></p>`
+            //Simple results
+            simpleResult.innerHTML += `<p class="author lead"><strong>${songTitle}</strong> Album by <span>${songArtist}</span> <button class="btn btn-success" onclick="getLyrics('${songArtist}','${songTitle}')" id="lyricsBtn">Get Lyrics</button></p>`
 
-            simpleResult.innerHTML += `<p class="author lead"><strong>${songTitle}</strong> Album by <span>${songArtist}</span> <button class="btn btn-success" onclick="getLyrics('${songArtist}','${songTitle}')"  id="lyricsBtn">Get Lyrics</button></p>`
-
-
-            //fancy
+            //Fancy results
             searchResult.innerHTML +=` <div class="single-result row align-items-center my-3 p-3">
             <div class="col-md-9">
                 <h3 class="lyrics-name">${songTitle}</h3>
@@ -42,12 +31,11 @@ const displaySong = (data)=>{
             <div class="col-md-3 text-md-right text-center">
                 <button onclick="getLyrics('${songArtist}','${songTitle}')" class="btn btn-success">Get Lyrics</button>
             </div>
-        </div>`
-            
+        </div>`      
     }
-   
 }
 
+//handled lyrics 
 function getLyrics(songArtist,songTitle) {
     const singleLyrics = document.getElementById('singleLyrics');
             fetch(`https://api.lyrics.ovh/v1/${songArtist}/${songTitle}`)
@@ -57,9 +45,11 @@ function getLyrics(songArtist,songTitle) {
                 singleLyrics.innerHTML += `<button class="btn go-back">&lsaquo;</button>
                 <h2 class="text-success mb-4">${songTitle} - ${songArtist}</h2>
                 <pre class="lyric text-white">${data.lyrics}</pre>`
+                simpleResult.innerHTML = '';
+                searchResult.innerHTML = '';
             })
+           
             singleLyrics.innerHTML = '';
-            // searchResult.innerHTML = '';
 }
 
 
